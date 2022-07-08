fb9cff2f75932f35b6fe9e26bc2373080561f5fa
brad@smithwebtek.com <brad@smithwebtek.com>
2022-07-07 23:20:26 -0400

    Add lorem;

--- src/About.js
+++ src/About.js
@@ -5,6 +5,7 @@ export default function About() {
   return (
     <div>
       <h1>About component</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed magnam iusto incidunt. Temporibus, eligendi! Eaque, ipsa atque quo tempore in, hic quasi tenetur cupiditate corporis, ex nostrum natus. Cumque, fuga.</p>
     </div>
   )
 }
